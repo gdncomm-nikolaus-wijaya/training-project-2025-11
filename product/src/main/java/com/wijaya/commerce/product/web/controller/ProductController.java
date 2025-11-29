3ae156c42fd26204a97f9b6bfd24e76c1d292332
GDN-COMMERCE\nikolaus.wijaya <nikolaus.wijaya@gdn-commerce.com>
2025-11-29 19:06:06 +0700

    add helper to convert the product restweb response

--- product/src/main/java/com/wijaya/commerce/product/web/controller/ProductController.java
+++ product/src/main/java/com/wijaya/commerce/product/web/controller/ProductController.java
@@ -11,6 +11,7 @@ import com.wijaya.commerce.product.commandImpl.Model.GetDetailProductCommandResp
 import com.wijaya.commerce.product.constant.ProductApiPath;
 import com.wijaya.commerce.product.restWebModel.response.GetDetailProductWebModel;
 import com.wijaya.commerce.product.restWebModel.response.WebResponse;
+import com.wijaya.commerce.product.service.util.ResponseHelper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,48 +31,7 @@ public class ProductController {
 
                 return WebResponse.<GetDetailProductWebModel>builder()
                                 .success(true)
-                                .data(mapToWebModel(commandResponse))
+                                .data(ResponseHelper.toGetDetailProductWebResponse(commandResponse))
                                 .build();
         }
-
-        private GetDetailProductWebModel mapToWebModel(GetDetailProductCommandResponse response) {
-                return GetDetailProductWebModel.builder()
-                                .sku(response.getSku())
-                                .name(response.getName())
-                                .description(response.getDescription())
-                                .brand(response.getBrand())
-                                .price(response.getPrice())
-                                .comparePrice(response.getComparePrice())
-                                .discountPercentage(response.getDiscountPercentage())
-                                .images(mapImages(response.getImages()))
-                                .specifications(response.getSpecifications())
-                                .categories(mapCategories(response.getCategories()))
-                                .build();
-        }
-
-        private java.util.List<GetDetailProductWebModel.ProductImage> mapImages(
-                        java.util.List<com.wijaya.commerce.product.commandImpl.Model.GetDetailProductCommandResponse.ProductImage> images) {
-                if (images == null)
-                        return null;
-                return images.stream()
-                                .map(img -> GetDetailProductWebModel.ProductImage.builder()
-                                                .url(img.getUrl())
-                                                .alt(img.getAlt())
-                                                .isPrimary(img.getIsPrimary())
-                                                .build())
-                                .collect(java.util.stream.Collectors.toList());
-        }
-
-        private java.util.List<GetDetailProductWebModel.CategoryInfo> mapCategories(
-                        java.util.List<com.wijaya.commerce.product.commandImpl.Model.GetDetailProductCommandResponse.CategoryInfo> categories) {
-                if (categories == null)
-                        return null;
-                return categories.stream()
-                                .map(cat -> GetDetailProductWebModel.CategoryInfo.builder()
-                                                .id(cat.getId())
-                                                .name(cat.getName())
-                                                .build())
-                                .collect(java.util.stream.Collectors.toList());
-        }
-
 }
